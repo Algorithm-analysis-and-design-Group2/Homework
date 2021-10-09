3c7985cf87af3339defe9f14ca3f0888639d0b04
赤月 <2256949562@qq.com>
2021-10-09 11:46:51 +0800

    算法分析与设计第一次作业

---
+++ 作业1/1_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int A[] = {1,2,3,4,5,6,7,8,9,10,11};
+int B[11];
+
+void print(int k) {
+	int i;
+	for (i = 0; i < k - 1; i++) {
+		cout << B[i] << " ";
+	}
+	cout << B[k - 1] << endl;
+}
+void solve(int sum,int t,int k) {
+
+	int i;
+	if (sum>15) {
+;		return;
+	}
+	if (sum == 15) {
+		print(k);
+		return;
+	}
+	for (i = t; i < 11; i++) {
+		
+		if (A[i] + sum <= 15) {
+			B[k] = A[i];
+			solve(A[i] + sum, i + 1, k + 1);
+		}
+	}
+}
+int main() {
+	solve(0, 0, 0);
+	return 0;
+}
